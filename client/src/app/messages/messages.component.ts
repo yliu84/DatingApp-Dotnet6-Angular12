c57f49bf9e176e8eb08d1ba3ac2980a397d83516
yliu84 <yliu84@studentmail.nait.ca>
2022-07-22 15:07:13 -0600

    updated pagination

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -43,8 +43,10 @@ export class MessagesComponent implements OnInit {
   }
 
   pageChanged(event: any) {
-    this.pageNumber = event.page;
-    this.loadMessages();
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
   }
 
 }
